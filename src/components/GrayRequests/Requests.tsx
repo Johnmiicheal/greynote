62692ca0afc5da658a00640f083a5fccf8f962a9
johnmiicheal <michealelijah301@gmail.com>
2023-05-16 22:43:47 +0100

    [graybook]: finishing touches

---
+++ src/components/GrayRequests/Requests.tsx
@@ -0,0 +1,121 @@
+import {
+  Image,
+  Text,
+  Flex,
+  Box,
+  Heading,
+  Avatar,
+  Badge,
+  Stack,
+  VStack,
+  Button,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+import { useMeQuery, Exact, Requests } from "../../gql/graphql";
+import { useRouter } from "next/router";
+import { formatDistanceToNow } from "date-fns";
+
+interface RequestProps {
+  p: Exact<Requests>;
+}
+
+const Requests: React.FC<RequestProps> = ({ p }) => {
+  const router = useRouter();
+  const [{ data: me }] = useMeQuery();
+  const admin = me?.me?.admin!;
+  const variant = () => {
+    if (p?.status === "PENDING") {
+      return "yellow";
+    } else if (p?.status === "APPROVED") {
+      return "teal";
+    } else if (p?.status === "REJECTED") {
+      return "red";
+    } else {
+      return "green";
+    }
+  };
+  return (
+    <VStack spacing={{ base: 0, md: 5 }}>
+      <Box
+        as="article"
+        borderWidth="1px"
+        borderRadius="lg"
+        bg="white"
+        borderColor="#F4B95F"
+        _hover={{ borderColor: "gray.400" }}
+        w={{ base: "full", lg: "750px" }}
+        mb={{ base: 2 }}
+      >
+        <Stack spacing={1} px={4}>
+          <Flex py={4} px={1} overflow="hidden">
+            <Avatar
+              src={p?.student?.school?.school?.logoImgUrl}
+              size="md"
+              mr={1}
+            />
+            <Flex direction="column">
+              <Text fontWeight={600}>
+                {p?.student?.school?.school?.schoolName}
+              </Text>
+              <Text>
+                Request for {p?.student?.firstName} {p?.student?.lastName}
+              </Text>
+            </Flex>
+            <Flex justify="center" gap={2} ml="auto">
+              <Text fontSize={12} fontWeight={300} noOfLines={2}>
+                {formatDistanceToNow(new Date(p?.createdAt), {
+                    addSuffix: true,
+                    includeSeconds: true,
+                })}
+              </Text>
+                <Box>
+                  <Badge variant="outline" colorScheme={variant()}>
+                    {p?.status}
+                  </Badge>
+                </Box>
+            </Flex>
+          </Flex>
+          <Flex justify="space-between" align="end">
+            <Box
+              mt={4}
+              px={2}
+              py={4}
+              bgColor="gray.200"
+              minW="30rem"
+              display={p.message.length > 1 ? "block" : "none"}
+            >
+              <Text fontSize={16} fontWeight={400} noOfLines={2}>
+                {p?.message}
+              </Text>
+            </Box>
+            <Flex gap={2}>
+              <Button
+                size="sm"
+                bg="#F4B95F"
+                color="white"
+                _hover={{ bg: "#DAA65D" }}
+                fontWeight={400}
+                borderRadius="3px"
+                onClick={() => router.push("/app/settings")}
+              >
+                Withdraw Request
+              </Button>
+            </Flex>
+          </Flex>
+
+          <Box
+            maxW="full"
+            alignItems="center"
+            mt={2}
+            zIndex={2}
+            display={me?.me?.admin ? "block" : "none"}
+          ></Box>
+        </Stack>
+      </Box>
+    </VStack>
+  );
+};
+
+export default Requests;
---
+++ src/components/GrayRequests/SmallRequests.tsx
@@ -0,0 +1,139 @@
+import {
+    Image,
+    Text,
+    Flex,
+    Box,
+    Heading,
+    Avatar,
+    Badge,
+    Stack,
+    VStack,
+    Button,
+    useDisclosure,
+    LinkOverlay,
+  } from "@chakra-ui/react";
+  import React from "react";
+  import { useMeQuery, Exact, Requests, useAcceptRequestMutation, useRejectRequestMutation } from "../../gql/graphql";
+  import { useRouter } from "next/router";
+  import { formatDistanceToNow } from "date-fns";
+import { TransferStudent } from "../Modals/TransferStudent";
+  
+  interface RequestProps {
+    p: Exact<Requests>;
+  }
+  
+  const SmallRequests: React.FC<RequestProps> = ({ p }) => {
+    const router = useRouter();
+    const [{ data: me }] = useMeQuery();
+    const [, accepted] = useAcceptRequestMutation();
+    const [, rejected] = useRejectRequestMutation();
+    const admin = me?.me?.admin!;
+    const variant = () => {
+      if (p?.status === "PENDING") {
+        return "yellow";
+      } else if (p?.status === "APPROVED") {
+        return "teal";
+      } else if (p?.status === "REJECTED") {
+        return "red";
+      } else {
+        return "green";
+      }
+    };
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const handleAcceptRequest = () => {
+        onOpen();
+        accepted({
+            acceptRequestId: p.id
+        })
+    };
+    const handleRejectRequest = () => {
+        rejected({
+            rejectRequestId: p.id
+        })
+    }
+    return (
+        <>
+      <VStack spacing={{ base: 0, md: 5 }}>
+        <Box
+          as="article"
+          borderWidth="1px"
+          borderRadius="lg"
+          bg="white"
+          borderColor="#F4B95F"
+          _hover={{ borderColor: "gray.400" }}
+          w={{ base: "full", lg: "320px" }}
+          mb={{ base: 2 }}
+        >
+          <Stack spacing={1} px={2}>
+            <Flex py={2} px={1} align="center" overflow="hidden">
+              <Avatar
+                src={p?.student?.school?.school?.logoImgUrl}
+                size="md"
+                mr={1}
+              />
+              <Flex direction="column" textAlign="start">
+                <Text fontWeight={600} fontSize={13}>
+                  {p?.student?.school?.school?.schoolName}
+                </Text>
+                <Text fontSize={12}>
+                  Request for {p?.student?.firstName} {p?.student?.lastName}
+                </Text>
+              </Flex>
+            </Flex>
+            <Flex justify="space-between" direction="column" align="start">
+              <Box
+                px={2}
+                py={2}
+                bgColor="gray.200"
+                w="full"
+                textAlign="start"
+                display={p.message.length > 1 ? "block" : "none"}
+              >
+                <Text fontSize={16} fontWeight={400} noOfLines={2}>
+                  {p?.message}
+                </Text>
+              </Box>
+              <Flex gap={2} mt={2}>
+                <Button
+                  size="sm"
+                  bg="#F4B95F"
+                  color="white"
+                  _hover={{ bg: "#DAA65D" }}
+                  fontWeight={400}
+                  borderRadius="3px"
+                  onClick={handleAcceptRequest}
+                >
+                  Accept
+                </Button>
+                <Button
+                  size="sm"
+                  border=" 1px solid #F4B95F"
+                  bg="white"
+                  color="#F4B95F"
+                  _hover={{ bg: "#DAA65D", color: "white" }}
+                  fontWeight={400}
+                  borderRadius="3px"
+                  onClick={handleRejectRequest}
+                >
+                  Reject
+                </Button>
+              </Flex>
+            </Flex>
+  
+            <Box
+              maxW="full"
+              alignItems="center"
+              mt={2}
+              zIndex={2}
+              display={me?.me?.admin ? "block" : "none"}
+            ></Box>
+          </Stack>
+        </Box>
+      </VStack>
+      <TransferStudent isOpen={isOpen} onClose={onClose} id={p?.student?.id} />
+      </>
+    );
+  };
+  
+  export default SmallRequests;
+  
